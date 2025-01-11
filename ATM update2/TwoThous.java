430246e2d998297e54c8e8888930568d5f25c197
Srisivalakshmi <srisivalakshmi.p23@gmail.com>
2025-01-11 23:30:08 +0530

    Delete ATM update2 directory

--- ATM update2/TwoThous.java
+++
@@ -1,8 +0,0 @@
-public class TwoThous extends Notes{
-
-    // Constructor to initialize the note value and count for ₹2000 notes.
-    public TwoThous(int note, int count){
-        // Constructor to initialize the note value and count for ₹2000 notes.
-        super(note, count);
-    }
-}
